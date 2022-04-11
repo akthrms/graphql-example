258b0cdf87ae742286860a816c2ee511c503bd88
akthrms <akt.hrms@gmail.com>
2022-04-11 13:23:08 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import { ApolloServer, gql } from "apollo-server";
-
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book!]!
-  }
-`;
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { ApolloServer } from "apollo-server";
+import { join } from "path";
 
 let books = [
   {
@@ -22,13 +15,19 @@ let books = [
   },
 ];
 
+const schema = loadSchemaSync(join(__dirname, "./../schema.graphql"), {
+  loaders: [new GraphQLFileLoader()],
+});
+
 const resolvers = {
   Query: {
     books: () => books,
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const schemeWithResolvers = addResolversToSchema({ schema, resolvers });
+
+const server = new ApolloServer({ schema: schemeWithResolvers });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
